e36f0014a173f08ccc1dd0dabc67e65c28d6e0ad
Carsten <carsten@bircks.dk>
2023-02-15 13:05:14 +0100

    Second commit

--- secrets.py
+++ secrets.py
@@ -5,5 +5,5 @@ secret = os.environ.get("SECRET")
 envvar = os.environ.get("ENV_VARIABLE")
 
 
-print(f"secret = {secrets.secret}")
-print(f"envvar = {variables.envvar}")
+print(f"secret = {secret}")
+print(f"envvar = {envvar}")
